ca70a2988f035e66463b2bd6c48137658c4c9525
igavelyuk <gavelyukworking@gmail.com>
2019-04-15 02:44:57 +0300

    added Instagram Tag And Like user.js file

---
+++ InstagramTagAndLike.user.js
@@ -0,0 +1,57 @@
+// ==UserScript==
+// @name Instagram Tag And Like
+// @description instagram.com like by tag name, auto like by tag name, best ever!
+// @namespace Violentmonkey Scripts
+// @match https://*.instagram.com/*
+// https://www.instagram.com/*/*
+// https://www.instagram.com/*/*/*
+// https://www.instagram.com/*/*/*/*
+// @installURL https://github.com/igavelyuk/CyanTigerGreasyforkScripts/raw/master/InstagramTagAndLike.user.js
+// @updateURL https://github.com/igavelyuk/CyanTigerGreasyforkScripts/raw/master/InstagramTagAndLike.user.js
+// @downloadURL https://github.com/igavelyuk/CyanTigerGreasyforkScripts/raw/master/InstagramTagAndLike.user.js
+// @grant none
+// @version 0.0.9
+// ==/UserScript==
+
+var likes = 0; // initial likes set to 0, result will be visible in console log (ctrl+shift+i, click console)
+var scrooltobottom = 200; //we will scrool bottom 200px
+
+// Just checking if page loaded an
+// Find this face it where program jump to start <(O.O)>
+window.addEventListener('DOMContentLoaded', function() {
+  console.log('window - DOMContentLoaded - bubble'); // 3rd
+function simulateCliks(el, evntType){
+  if (el.fireEvent) {
+    el.fireEvent('on' + evntType);
+  } else {
+    var evObj = document.createEvent('Events');
+    evObj.initEvent(evntType, true, false);
+    el.dispatchEvent(evObj);
+  }
+  likes++;
+}
+//  <span class="glyphsSpriteHeart__filled__24__red_5 Szr5J">Unlike</span>
+//  <span class="glyphsSpriteHeart__outline__24__grey_9 Szr5J">Like</span>
+var greyHearts = [];   //	Array of unliked things
+var likeButton = [];   //	Array of buttons
+var scrooltobottom =0;
+var fixedScrool = 1600; //	scrool for 600 px
+var timerInt = 7000; //	7 sec
+
+// <(O.O)> starting here with timer
+  setInterval(function(){
+    // Selecting all not "Liked Hearts"
+    // So we will click "Like" and "Next"
+    // Each time then pass simulateCliks function    dCJp8 afkep coreSpriteHeartOpen _0mzm-"
+    greyHearts = document.getElementsByClassName("dCJp8");
+    simulateCliks(greyHearts[0], 'click', likes);
+// Finding right buttons
+    var xCom = document.getElementsByClassName("coreSpriteRightPaginationArrow");
+    console.log(xCom[0]);
+    simulateCliks(xCom[0], 'click', likes);
+
+    // Ctrl+Shift+I
+    // Find Console and see stats
+    // Thats all folks
+  }, timerInt);
+});
