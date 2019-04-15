1ea2f64cbc54e3db96ca2cffe6cf76ae0708b1bf
igavelyuk <gavelyukworking@gmail.com>
2019-04-15 12:58:25 +0300

    added Instagram Feed Auto Like user.js

---
+++ InstagramFeedAutoLike.user.js
@@ -0,0 +1,57 @@
+// ==UserScript==
+// @name Instagram Feed Auto Like
+// @description instagram.com like user feed, auto likeer, best ever!
+// @namespace Violentmonkey Scripts
+// @match https://*.instagram.com/*
+// https://www.instagram.com/*/*
+// https://www.instagram.com/*/*/*
+// https://www.instagram.com/*/*/*/*
+// @installURL https://github.com/igavelyuk/CyanTigerGreasyforkScripts/raw/master/InstagramTagAndLike.user.js
+// @updateURL https://github.com/igavelyuk/CyanTigerGreasyforkScripts/raw/master/InstagramTagAndLike.user.js
+// @downloadURL https://github.com/igavelyuk/CyanTigerGreasyforkScripts/raw/master/InstagramTagAndLike.user.js
+// @grant none
+// @version 0.0.9
+// ==/UserScript==
+
+
+var likes = 0; // initial likes set to 0, result will be visible in console log (ctrl+shift+i, click console)
+var scrooltobottom = 200; //we will scrool bottom 200px
+
+// Just checking if page loaded an
+// Find this face it where program jump to start <(O.O)>
+window.addEventListener('DOMContentLoaded', function() {
+  console.log('window - DOMContentLoaded - bubble'); // 3rd
+function simulateCliks(el, evntType){
+  if (el.fireEvent) {
+    el.fireEvent('on' + evntType);
+  } else {
+    var evObj = document.createEvent('Events');
+    evObj.initEvent(evntType, true, false);
+    el.dispatchEvent(evObj);
+  }
+  likes++;
+}
+//  <span class="glyphsSpriteHeart__filled__24__red_5 Szr5J">Unlike</span>
+//  <span class="glyphsSpriteHeart__outline__24__grey_9 Szr5J">Like</span>
+var greyHearts = [];   //	Array of unliked things
+var likeButton = [];   //	Array of buttons
+var scrooltobottom =0;
+var fixedScrool = 1600; //	scrool for 600 px
+var timerInt = 7000; //	7 sec
+
+// <(O.O)> starting here with timer
+  setInterval(function(){
+    // Selecting all not "Liked Hearts"
+    // So we will click "Like" and "Next"
+    // Each time then pass simulateCliks function
+    greyHearts = document.getElementsByClassName("glyphsSpriteHeart__outline__24__grey_9");
+    simulateCliks(greyHearts[0].parentElement, 'click', likes);
+		// Finding right buttons
+    window.scrollTo(0,scrooltobottom);
+    scrooltobottom=scrooltobottom+fixedScrool;
+    console.log(likes+"-- likes for this session");
+    // Ctrl+Shift+I
+    // Find Console and see stats
+    // Thats all folks
+  }, timerInt);
+});
